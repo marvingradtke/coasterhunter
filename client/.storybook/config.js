a7912780d38280562f047d4cc3d3c91e06174ceb
marvingradtke <m.gradtke@gmail.com>
2019-12-19 10:19:48 +0100

    Prepare theming and global styles

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import GlobalStyles from '../src/GlobalStyles';
 import { withKnobs } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'emotion-theming';
+import main from '../src/themes/defaultTheme';
 
 // add GlobalStyle for every story
 const GlobalStyleDecorator = storyFn => (
-  <>
+  <ThemeProvider theme={main}>
     <GlobalStyles />
     {storyFn()}
-  </>
+  </ThemeProvider>
 );
 addDecorator(GlobalStyleDecorator);
 addDecorator(withKnobs);
---
+++ client/src/themes/defaultTheme.js
@@ -0,0 +1,12 @@
+const main = {
+  colors: {
+    background:
+      'linear-gradient(90deg,rgba(73, 193, 203, 1) 0%,rgba(178, 228, 167, 1) 50%,rgba(189, 239, 171, 1) 100%)',
+    primary: '#707070',
+    secondary: '#FFFFFF',
+    highlight: '#4CC1CB',
+    problem: '#FD6A6A'
+  }
+};
+
+export default main;
