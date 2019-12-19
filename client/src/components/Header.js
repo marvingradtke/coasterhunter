c5ffa060f6c4cd135d490789e81286b260147f5b
marvingradtke <m.gradtke@gmail.com>
2019-12-19 17:31:55 +0100

    Add header with logo, search button and burger menu

---
+++ client/src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Logo from '../assets/coasterhunter-logo.svg';
+import SearchIcon from '../assets/icon-search.svg';
+import MenuIcon from '../assets/icon-feather.svg';
+
+const Header = styled.header`
+  display: flex;
+  background-color: ${props => props.theme.colors.background};
+  justify-content: space-between;
+  height: 100px;
+  width: 100%;
+`;
+
+const Icon = styled.img`
+  width: 30px;
+`;
+
+const HeadLogo = styled.img`
+  display: flex;
+  height: 120px;
+  margin-top: 10px;
+`;
+
+const HeadButton = styled.button`
+  width: 45px;
+  height: 45px;
+  margin: 15px;
+  background: none;
+  border: none;
+`;
+
+export default function HeaderNav() {
+  return (
+    <Header>
+      <HeadButton>
+        <Icon src={SearchIcon}></Icon>
+      </HeadButton>
+      <HeadLogo src={Logo}></HeadLogo>
+      <HeadButton>
+        <Icon src={MenuIcon}></Icon>
+      </HeadButton>
+    </Header>
+  );
+}
