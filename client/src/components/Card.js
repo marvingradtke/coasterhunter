9a4f0adf0917eaec398a8027984c401a50bd9845
marvingradtke <m.gradtke@gmail.com>
2019-12-20 15:32:26 +0100

    Add img to card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import Coaster from '../assets/coaster-taron.jpg';
 
 const CardWrap = styled.div`
   filter: drop-shadow(2px 3px 3px rgba(50, 50, 0, 0.2));
@@ -7,19 +8,39 @@ const CardWrap = styled.div`
 
 const Card = styled.div`
   display: flex;
+  flex-direction: column;
   background-color: ${props => props.theme.colors.secondary};
-  opacity: 0.7;
-  box-shadow: 10px 10px black;
   width: 300px;
-  height: 150px;
+  height: 200px;
   margin: auto;
-  clip-path: polygon(10% 0, 100% 0, 100% 0, 100% 80%, 90% 100%, 0 100%, 0 100%, 0% 20%);
+  clip-path: polygon(10% 0, 100% 0, 100% 0, 100% 85%, 90% 100%, 0 100%, 0 100%, 0 15%);
+`;
+
+const CardBox = styled.div`
+  display: flex;
+  background-color: ${props => props.theme.colors.secondary};
+  height: 100px;
+  width: 300px;
+  margin: auto;
+  position: absolute;
+  top: 80px;
+`;
+
+const Image = styled.img`
+  display: flex;
+  width: 100%;
+  position: relative;
 `;
 
 export default function CardComponent() {
   return (
     <CardWrap>
-      <Card></Card>
+      <Card>
+        <Image src={Coaster}></Image>
+        <CardBox>
+          <h2>Taron</h2>
+        </CardBox>
+      </Card>
     </CardWrap>
   );
 }
