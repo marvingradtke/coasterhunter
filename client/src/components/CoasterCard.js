3a51b2b088398662dccaf0c0aaa18aa2e816cd2c
marvingradtke <m.gradtke@gmail.com>
2020-01-02 14:19:43 +0100

    Map wagon images

---
+++ client/src/components/CoasterCard.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import coaster from '../assets/coaster-taron.jpg';
+import wagon from '../assets/coaster-wagon.svg';
+
+const Shadow = styled.div`
+  filter: drop-shadow(2px 3px 3px rgba(50, 50, 0, 0.2));
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  color: ${props => props.theme.colors.primary};
+  background-color: rgba(255, 255, 255, 0.8);
+  width: 300px;
+  height: 200px;
+  margin: auto;
+  margin-top: 10px;
+  clip-path: polygon(10% 0, 100% 0, 100% 0, 100% 85%, 90% 100%, 0 100%, 0 100%, 0 15%);
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 40%;
+  object-fit: cover;
+`;
+
+const WagonImage = styled.img`
+  width: 24px;
+  margin: 1px;
+`;
+
+const CardFooter = styled.div`
+  display: flex;
+  margin: 5px;
+`;
+
+const Name = styled.h2`
+  margin: 0px;
+  flex-grow: 1;
+`;
+
+const Ranking = styled.span`
+  color: ${props => props.theme.colors.highlight};
+  font-size: 1.5em;
+`;
+
+const RankingFix = styled.span`
+  font-size: 1.5em;
+`;
+
+const CardContent = styled.div`
+  display: flex;
+  margin: 0px 5px;
+  width: 100%;
+`;
+
+export default function CoasterCard() {
+  return (
+    <Shadow>
+      <Card>
+        <Image src={coaster} />
+        <CardContent>
+          <Name>Taron</Name>
+          <Ranking>6.3</Ranking>
+          <RankingFix>/8</RankingFix>
+        </CardContent>
+        <CardFooter>
+          {[1, 2, 3, 4, 5, 6, 7, 8].map(ranking => (
+            <WagonImage key={ranking} src={wagon} />
+          ))}
+        </CardFooter>
+      </Card>
+    </Shadow>
+  );
+}
---
+++ client/src/stories/CoasterCard.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CoasterCard from '../components/CoasterCard';
+
+export default {
+  title: 'Card'
+};
+
+export function Example() {
+  return <CoasterCard />;
+}
